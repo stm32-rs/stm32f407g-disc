74093d00e2447718abd462d9a37b6eb164774b2f
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2023-10-26 12:08:17 -0700

    Upgrade accelerometer.rs to hal 0.10

--- src/accelerometer.rs
+++ src/accelerometer.rs
@@ -9,15 +9,12 @@ use crate::hal::rcc;
 use crate::hal::spi;
 use crate::hal::stm32;
 
-use embedded_hal;
-use embedded_hal::digital::v2::OutputPin;
-
 type Spi1 = spi::Spi<
     stm32::SPI1,
     (
-        gpioa::PA5<gpio::Alternate<gpio::AF5>>,
-        gpioa::PA6<gpio::Alternate<gpio::AF5>>,
-        gpioa::PA7<gpio::Alternate<gpio::AF5>>,
+        gpioa::PA5<gpio::Alternate<{ gpio::AF5 }>>,
+        gpioa::PA6<gpio::Alternate<{ gpio::AF5 }>>,
+        gpioa::PA7<gpio::Alternate<{ gpio::AF5 }>>,
     ),
     spi::TransferModeNormal,
 >;
@@ -35,19 +32,19 @@ impl Accelerometer {
         spi1: stm32::SPI1,
         clocks: rcc::Clocks,
     ) -> Self {
-        let sck = gpioa.pa5.into_alternate_af5().internal_pull_up(false);
-        let miso = gpioa.pa6.into_alternate_af5().internal_pull_up(false);
-        let mosi = gpioa.pa7.into_alternate_af5().internal_pull_up(false);
+        let sck = gpioa.pa5.into_alternate().internal_pull_up(false);
+        let miso = gpioa.pa6.into_alternate().internal_pull_up(false);
+        let mosi = gpioa.pa7.into_alternate().internal_pull_up(false);
 
         let spi_mode = spi::Mode {
             polarity: spi::Polarity::IdleLow,
             phase: spi::Phase::CaptureOnFirstTransition,
         };
 
-        let spi = spi::Spi::spi1(spi1, (sck, miso, mosi), spi_mode, 10.mhz().into(), clocks);
+        let spi = spi::Spi::new(spi1, (sck, miso, mosi), spi_mode, 10.mhz().into(), clocks);
 
         let mut chip_select = gpioe.pe3.into_push_pull_output();
-        chip_select.set_high().ok();
+        chip_select.set_high();
 
         let config = lis302dl::Config {
             scale: lis302dl::Scale::PlusMinus8G,
