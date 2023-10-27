fc9d0181daeea4b57cc0cdaf0552d76ab26dc2ac
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2023-10-26 20:15:26 -0700

    Update the pin set_low set_high toggle to hal 0.10

--- src/led.rs
+++ src/led.rs
@@ -114,20 +114,16 @@ ctor!(LD3, LD4, LD5, LD6);
 impl Led {
     /// Turns the LED off
     pub fn off(&mut self) {
-        self.pin.set_low().ok();
+        self.pin.set_low();
     }
 
     /// Turns the LED on
     pub fn on(&mut self) {
-        self.pin.set_high().ok();
+        self.pin.set_high();
     }
 
     /// Toggles the LED
     pub fn toggle(&mut self) {
-        if let Ok(true) = self.pin.is_low() {
-            self.pin.set_high().ok();
-        } else {
-            self.pin.set_low().ok();
-        }
+        self.pin.toggle();
     }
 }
