4781d0cc72fc669dbbae6ab3e21d6ef41b690f63
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2023-11-03 22:03:14 -0700

    Also fix the LED order for the accelerometer example

--- examples/mems.rs
+++ examples/mems.rs
@@ -11,8 +11,8 @@ use stm32f407g_disc as board;
 
 use cortex_m_rt::entry;
 
-use board::hal::prelude::*;
 use board::hal::pac;
+use board::hal::prelude::*;
 use board::led::{LedColor, Leds};
 
 use cortex_m::iprintln;
@@ -55,25 +55,25 @@ fn main() -> ! {
                 acceleration.z,
             );
 
-            // x+ orange
-            // x- blue
-            // y+ red
-            // y- green
+            // x+ red
+            // x- green
+            // y+ orange
+            // y- blue
 
             if acceleration.x > 0.0 {
-                leds[LedColor::Orange].on();
-                leds[LedColor::Blue].off();
-            } else {
-                leds[LedColor::Blue].on();
-                leds[LedColor::Orange].off();
-            }
-
-            if acceleration.y > 0.0 {
                 leds[LedColor::Red].on();
                 leds[LedColor::Green].off();
             } else {
                 leds[LedColor::Green].on();
                 leds[LedColor::Red].off();
+            }
+
+            if acceleration.y > 0.0 {
+                leds[LedColor::Orange].on();
+                leds[LedColor::Blue].off();
+            } else {
+                leds[LedColor::Blue].on();
+                leds[LedColor::Orange].off();
             }
         }
     }
